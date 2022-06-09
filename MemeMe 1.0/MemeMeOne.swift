d115ef292781115d8acbc29c42166cd74334e2a1
Dhruv <shah_dhruv@rocketmail.com>
2022-06-09 17:44:10 +0530

    Initial Commit

---
+++ MemeMe 1.0/MemeMeOne.swift
@@ -0,0 +1,17 @@
+//
+//  MemeMeOne.swift
+//  MemeMe 1.0
+//
+//  Created by Dhruv Shah on 29/07/21.
+//
+
+import UIKit
+
+struct MemeMeOne {
+    
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var newImage: UIImage
+    
+}
